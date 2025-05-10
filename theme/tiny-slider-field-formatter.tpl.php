3863460f2842cf400aec8edac86855848522c8ab
Rodobaldo <rperez@tanren.cr>
2025-05-10 18:59:17 -0400

    fixed xss issue

--- theme/tiny-slider-field-formatter.tpl.php
+++ theme/tiny-slider-field-formatter.tpl.php
@@ -1,16 +1,15 @@
-<?php if ($attributes): ?>
-<div data-settings='<?php print $attributes['data-settings']; ?>'
-  <?php if (!empty($attributes['class'])) {
-    print 'class="' . implode(' ', $attributes['class']) . '"';
-  } ?>>
-  <?php print $output; ?>
-  <?php endif; ?>
-  <?php if ($settings['navAsThumbnails'] == '1'): ?>
-    <ul class="thumbs-slider"
-        data-field-id="<?php print $settings['uniqueFieldID']; ?>">
-      <?php print $thumbs; ?>
-    </ul>
-  <?php endif; ?>
-  <?php if ($attributes): ?>
-</div>
+<?php if (!empty($attributes)): ?>
+  <div data-settings='<?php print check_plain($attributes['data-settings']); ?>'
+    <?php if (!empty($attributes['class'])): ?>
+      class="<?php print check_plain(implode(' ', $attributes['class'])); ?>"
+    <?php endif; ?>>
+    <?php print $output; ?>
+
+    <?php if (!empty($settings['navAsThumbnails']) && $settings['navAsThumbnails'] == '1'): ?>
+      <ul class="thumbs-slider"
+          data-field-id="<?php print check_plain($settings['uniqueFieldID']); ?>">
+        <?php print $thumbs; ?>
+      </ul>
+    <?php endif; ?>
+  </div>
 <?php endif; ?>
--- theme/tiny-slider-views.tpl.php
+++ theme/tiny-slider-views.tpl.php
@@ -1,16 +1,15 @@
-<?php if ($attributes): ?>
-<div data-settings='<?php print $attributes['data-settings']; ?>'
-  <?php if (!empty($attributes['class'])) {
-    print 'class="' . implode(' ', $attributes['class']) . '"';
-  } ?> >
-  <?php endif; ?>
-  <?php foreach ($rows as $row_count => $row): ?>
-    <div <?php if (!empty($row_classes[$row_count])) {
-      print 'class="' . implode(' ', $row_classes[$row_count]) . '"';
-    } ?>>
-      <?php print $row; ?>
-    </div>
-  <?php endforeach; ?>
-  <?php if ($attributes): ?>
-</div>
+<?php if (!empty($attributes)): ?>
+  <div data-settings='<?php print check_plain($attributes['data-settings']); ?>'
+    <?php if (!empty($attributes['class'])): ?>
+      class="<?php print check_plain(implode(' ', $attributes['class'])); ?>"
+    <?php endif; ?>>
+
+    <?php foreach ($rows as $row_count => $row): ?>
+      <div <?php if (!empty($row_classes[$row_count])): ?>
+        class="<?php print check_plain(implode(' ', $row_classes[$row_count])); ?>"
+      <?php endif; ?>>
+        <?php print $row; ?>
+      </div>
+    <?php endforeach; ?>
+  </div>
 <?php endif; ?>
